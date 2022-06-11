43fcf71e4e077e92d6b4f322ac2a76db8a66b53c
rim-buei <rim.buei@gmail.com>
2022-06-11 15:41:47 +0900

    rust/mandelbrot: update crossbeam

--- rust/mandelbrot/src/main.rs
+++ rust/mandelbrot/src/main.rs
@@ -163,11 +163,12 @@ fn main() {
                 let band_lower_right =
                     pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
 
-                spawner.spawn(move || {
+                spawner.spawn(move |_| {
                     render(band, band_bounds, band_upper_left, band_lower_right);
                 });
             }
         })
+        .unwrap()
     }
 
     write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
